42782ac11137f1840e9bd539844a1721c7eb0022
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-02 20:49:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,10 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+def age_appropriate_birthday(age)
+if age <= 12 
+  age.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
